c958ab61b0f96e7368569610ba7cd5ada2324042
Joseph Budin <jpbudi11@smumn.edu>
2013-02-19 08:01:52 -0600

    looks change

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -5,23 +5,25 @@ module OrdersHelper
 
 	def customer_order_information(customer)
 		out = ""
-		if customer.orders.first.nil?
+		if customer.orders.nil?
 		  
 			out = "<tr><td>No Orders Available</td></tr>"
 			
 		else
 			customer.orders.each do |order| 
 			  
-				link_info = link_to order_information(order), order
+				get_the_link = link_to information_of_the_order(order), order
 				
-				out << "<tr><td> Link To Order :" << link_info << "</td></tr>"
+				out << "<tr><td> Link To Order: " << get_the_link << "</td></tr>"
 			end
     	end
 		out.html_safe
 	end
 
-	def order_information(order)
+	def information_of_the_order(order)
 		out = order.customer_name << " Quantity = " << order.quantity.to_s << " Item Bought = " << order.item_name <<  "Price = " << order.total_cost.to_s
+		
+		
 		out.html_safe
 	end
 end
